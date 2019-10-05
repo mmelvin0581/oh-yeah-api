c9eb8d750064574a7e29d6216ce750c7c0c751bc
mmelvin0581 <mmelvin0581@gmail.com>
2019-10-04 22:23:02 -0400

    auth validation and error handling

--- src/auth/dto/auth.credentials.dto.ts
+++ src/auth/dto/auth.credentials.dto.ts
@@ -1,4 +1,17 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  // at least one number OR special char, one uppercase char, one lowercase char
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password is too weak',
+  })
   password: string;
 }
