87dcdeb64be0deb5dc08d1bed9a2a4b44e63f038
mmelvin0581 <mmelvin0581@gmail.com>
2019-10-05 23:11:31 -0400

    enable cors in development

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,10 @@ async function bootstrap() {
   const logger = new Logger('Main');
   const app = await NestFactory.create(AppModule);
 
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  }
+
   const port = process.env.PORT || serverConfig.port;
 
   app.setGlobalPrefix('api');
