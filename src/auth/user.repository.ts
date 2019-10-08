bbb3300a7637bcdf88223bf12691b2bf6cfbfc2b
mmelvin0581 <mmelvin0581@gmail.com>
2019-10-07 22:49:14 -0400

    test user.repository

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -9,11 +9,11 @@ export class UserRepository extends Repository<User> {
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
-    const user = new User();
+    const user = this.create();
 
     user.username = username;
     user.salt = await bcrypt.genSalt();
-    user.password = await UserRepository.hashPassword(password, user.salt);
+    user.password = await this.hashPassword(password, user.salt);
 
     try {
       await user.save();
@@ -37,7 +37,7 @@ export class UserRepository extends Repository<User> {
     return null;
   }
 
-  private static async hashPassword(password: string, salt: string): Promise<string> {
+  async hashPassword(password: string, salt: string): Promise<string> {
     return await bcrypt.hash(password, salt);
   }
 }
